c2c21550cf12cf1792e22f964acc7bd7d8d060f6
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-30 13:52:17 +0200

    setup next to handle requests

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require("express");
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
+const handle = app.getRequestHandler();
 
 const app = next({ dev });
 
 app.prepare().then(() => {
 	const server = express();
+
+	server.get("*", (req, res) => {
+		return handle(req, res);
+	});
 
 	server.listen(port, err => {
 		if (err) {

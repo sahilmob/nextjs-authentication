bcd2246484e2f6ec4186e90651d6478edda5d930
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-30 21:06:15 +0200

    import and implement logoutUser

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { logoutUser } from "../lib/auth";
 
 const Layout = ({ children, title, auth }) => {
 	const { user = {} } = auth || {};
@@ -17,7 +18,7 @@ const Layout = ({ children, title, auth }) => {
 							<Link href="/profile">
 								<a>Profile</a>
 							</Link>
-							<button>Logout</button>
+							<button onClick={logoutUser}>Logout</button>
 						</>
 					) : (
 						<Link href="/login">

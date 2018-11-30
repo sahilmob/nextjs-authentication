48c342570e99c4e8f2f946548e701b2c975dd3e1
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-30 14:52:49 +0200

    create login form

---
+++ components/LoginForm.js
@@ -0,0 +1,49 @@
+import { loginUser } from "../lib/auth";
+
+export class LoginForm extends React.Component {
+	state = {
+		email: "",
+		password: ""
+	};
+
+	handleChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	handleSubmit = event => {
+		const { email, password } = this.state;
+		event.preventDefault();
+		loginUser(email, password);
+	};
+
+	render() {
+		const { email, password } = this.state;
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<div>
+					<input
+						type="email"
+						name="email"
+						placeholder="email"
+						onChange={this.handleChange}
+						value={email}
+					/>
+				</div>
+				<div>
+					<input
+						type="password"
+						name="password"
+						placeholder="placeholder"
+						onChange={this.handleChange}
+						value={password}
+					/>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		);
+	}
+}
+
+export default LoginForm;

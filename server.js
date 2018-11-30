ea22f3aadbdefdc5f62f3f406c483f8baa9a5a5f
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-30 13:55:12 +0200

    fix getRequestHandler function

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
-const handle = app.getRequestHandler();
 
 const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
 	const server = express();

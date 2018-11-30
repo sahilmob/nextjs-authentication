6f801a59a8d52ffe523d3e01a281ff12c471d9e8
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-30 20:53:51 +0200

    render nav conditionally

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,29 @@
 import Link from "next/link";
 
-const Layout = ({ children, title }) => {
+const Layout = ({ children, title, auth }) => {
+	const { user = {} } = auth || {};
 	return (
 		<div className="root">
 			<nav className="navbar">
 				<span>
-					Welcome, <strong>Guest</strong>
+					Welcome, <strong>{user.name || "Guest"}</strong>
 				</span>
 				<div>
 					<Link href="/">
 						<a>Home</a>
 					</Link>
-					<Link href="/profile">
-						<a>Profile</a>
-					</Link>
-					<button>Logout</button>
-					<Link href="/login">
-						<a>Login</a>
-					</Link>
+					{user.email ? (
+						<>
+							<Link href="/profile">
+								<a>Profile</a>
+							</Link>
+							<button>Logout</button>
+						</>
+					) : (
+						<Link href="/login">
+							<a>Login</a>
+						</Link>
+					)}
 				</div>
 			</nav>
 			<h1>{title}</h1>
